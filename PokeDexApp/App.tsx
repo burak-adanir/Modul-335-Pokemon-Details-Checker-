cb2063f4b6aacaea10fa429e8407bbdce5dc2e8e
Emma Bright <113352515+emmabrght@users.noreply.github.com>
2024-06-12 14:22:32 +0200

    Reset package files
    makes resolving merge conflicts easier :)

--- PokeDexApp/App.tsx
+++ PokeDexApp/App.tsx
@@ -1,31 +1,13 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import PokemonCard from "./src/components/organisms/PokemonCard";
-import { useEffect, useState } from "react";
-import Pokemon from "./src/types/Pokemon";
-import PokemonService from "./src/services/PokemonService";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import PokeBallButton from "./src/components/atoms/PokeBallButton";
+import NavBar from "./src/components/molecules/NavBar";
 
 export default function App() {
-  const [filterdList, setFilteredList] = useState<Pokemon[]>([]);
-  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-
-  useEffect(() => {
-    PokemonService()
-      .getAll()
-      .then((value) => {
-        setPokemonList(value.data);
-        console.log(value);
-        setFilteredList(value.data);
-      })
-      .catch((error) => console.log(error));
-  });
-
   return (
     <View style={styles.container}>
-      <FlatList
-        data={pokemonList}
-        renderItem={({ item }) => <PokemonCard pokemonData={item} />}
-        keyExtractor={(item) => `${item.id}`}
-      />
+      <NavBar></NavBar>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -33,12 +15,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
-    padding: 10,
-  },
-  loader: {
-    flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#5C9CF7",
     alignItems: "center",
+    justifyContent: "center",
+  },
+  logoImage: {
+    width: "100%",
+    height: "15%",
+    marginTop: 32,
   },
 });
